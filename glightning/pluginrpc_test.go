a79a2f5f4660e37d500f0425b3afcae674981b24
lisa neigut <niftynei@gmail.com>
2019-08-14 11:26:03 -0500

    add tests for 'usage'

---
+++ glightning/pluginrpc_test.go
@@ -0,0 +1,39 @@
+package glightning_test
+
+import (
+	"fmt"
+	"github.com/niftynei/glightning/glightning"
+	"github.com/niftynei/glightning/jrpc2"
+	"testing"
+)
+
+type ParamMethod struct {
+	Required string `json:"required"`
+	Optional string `json:"optional,omitempty"`
+}
+
+func (p *ParamMethod) Name() string {
+	return "param-test"
+}
+
+func (p *ParamMethod) New() interface{} {
+	return &ParamMethod{}
+}
+
+func (p *ParamMethod) Call() (jrpc2.Result, error) {
+	return fmt.Sprintf("Called with %s and [%s]", p.Required, p.Optional), nil
+}
+
+
+func TestManifestWithUsage(t *testing.T) {
+	initFn := getInitFunc(t, func(t *testing.T, options map[string]string, config *glightning.Config) {
+		t.Error("Should not have called init when calling get manifest")
+	})
+	plugin := glightning.NewPlugin(initFn)
+	plugin.RegisterMethod(glightning.NewRpcMethod(&ParamMethod{}, "Call a param"))
+
+	msg := "{\"jsonrpc\":\"2.0\",\"method\":\"getmanifest\",\"id\":\"aloha\"}\n\n"
+	resp := "{\"jsonrpc\":\"2.0\",\"result\":{\"options\":[],\"rpcmethods\":[{\"name\":\"param-test\",\"description\":\"Call a param\",\"usage\":\"required [optional]\"}],\"dynamic\":true},\"id\":\"aloha\"}"
+	runTest(t, plugin, msg, resp)
+}
+
